00d0c144493cd3cd4f1f926ca6d8deb959128f06
Rommel Terrence Juanillo <35565900+renceInbox@users.noreply.github.com>
2019-10-01 03:01:06 +0000

    setup fingerjet plugin for OpenAbis

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='openabis-fingerjetfx',
+    version='0.0.1',
+    packages=['openabis_fingerjetfx'],
+    url='https://github.com/newlogic42/openabis-fingerjetfx',
+    license='Apache License',
+    author='newlogic42',
+    author_email='',
+    description='OpenAbis\' plugin implementation of FingerJetFXOSE/FingerJetFXOSE.'
+)
